f5ba12677ad468702de9b05c02a71147fed15a7f
donovanpoland <dpoland@byupathway.edu>
2025-07-12 00:20:36 -0600

    changed background color / changed button and link bg color / added a word to "business add" / adjust bg changes on site plan / added listeners for grid or list views

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,20 +3,53 @@
 const url = `data/members.json`;
 // Find directory insertion class in html
 const cards = document.querySelector(".directory");
+const gridView = document.querySelector("#grid-view");
+const listView = document.querySelector("#list-view");
+
 
 // Load file
 async function getMemberData() {
+    
     // Get data from json file
     const response = await fetch(url);
     const data = await response.json();
     // Log data(jason file) to console in table format to the console
     console.table(data.members);
-    displayMembers(data.members);
+
+    // Default selection
+    displayGrid(data.members);
+
+    // If user selects list view
+    listView.addEventListener("click", () => {
+        // Adjust aria attributes
+        gridView.setAttribute("aria-pressed", "false");
+        listView.setAttribute("aria-pressed", "true");
+
+        // Erase all old data
+        document.querySelector(".directory").innerHTML = "";
+
+        // Replace with grid data
+        displayList(data.members);
+    });
+
+     // If user selects grid view
+    gridView.addEventListener("click", () => {
+        // Adjust aria attributes
+        gridView.setAttribute("aria-pressed", "true");
+        listView.setAttribute("aria-pressed", "false");
+
+        // Erase all old data
+        document.querySelector(".directory").innerHTML = "";
+
+        // Replace with grid data
+        displayGrid(data.members);
+    });
+        
 }
 
 getMemberData();
 
-function displayMembers(members) {
+function displayGrid(members) {
     members.forEach(member => {
         // Create a new card
         const card = document.createElement("section");
@@ -44,7 +77,6 @@ function displayMembers(members) {
             `${member.address.street}<br>${member.address.city}
                 ${member.address.state}, ${member.address.zip}`;
         
-
         // Phone
         const phone = document.createElement("p");
         phone.textContent = member.phone;
@@ -60,13 +92,46 @@ function displayMembers(members) {
         addTag.appendChild(phone);
         addTag.appendChild(website);
         
-
         // Add address tag to the card
         card.appendChild(name);
         card.appendChild(img);
         card.appendChild(addTag)
 
         // Add the card to the page
+        cards.appendChild(card);
+    });
+}
+
+
+function displayList(members) {
+    members.forEach(member => {
+        const cards = document.querySelector(".directory");
+        cards.style.display = "flex"
+        // Create a new card
+        const card = document.createElement("section")
+        card.classList.add("card");
+        card.style.flexDirection = "row";
+
+        const items = [
+            member.name,
+            `${member.address.street}<br>${member.address.city}
+                ${member.address.state}, ${member.address.zip}`,
+            member.phone,
+            member.website
+        ];
+
+        const list = document.createElement("ul");
+
+        items.forEach((item) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = item;
+            list.appendChild(listItem);
+        });
+        
+        // Add to card
+        card.appendChild(list);
+
+        // Add to page
         cards.appendChild(card);
     });
 }
