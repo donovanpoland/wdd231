7fe32d1727f7922fe19e35c1960adc25c24d83ff
donovanpoland <dpoland@byupathway.edu>
2025-07-12 18:16:49 -0600

    hamburger menu /mobile and desktop optimization / navigation corrections /spacing corrections

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,15 @@
 const navButton = document.querySelector('#nav-button');
 const navBar = document.querySelector('#nav-bar');
+let width = window.innerWidth;
+
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navBar.classList.toggle('show');
+});
+
+// Listen for window resize and remove show class
+window.addEventListener("resize", () => {
+  navButton.classList.remove('show');
+  navBar.classList.remove('show');
 });
