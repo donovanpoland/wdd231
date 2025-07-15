1d3dfde0e6209820fcca6c5b2bfe56b94b85b155
donovanpoland <dpoland@byupathway.edu>
2025-07-15 00:15:43 -0600

    links to learning projects from WDD213

--- modules/scripts/output.mjs
+++ modules/scripts/output.mjs
@@ -6,11 +6,11 @@ export function setTitle(course) {
 }
 
 export function renderSections(sections) {
-    const html = sections.map(
+    const options = sections.map(
         (section) => `<tr>
         <td>${section.sectionNumber}</td>
         <td>${section.enrolled}</td>
         <td>${section.instructor}</td></tr>`
     );
-    document.querySelector("#sections").innerHTML = html.join("");
+    document.querySelector("#sections").innerHTML = options.join("");
 }
