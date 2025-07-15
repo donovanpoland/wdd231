760a76a32a43d5228b71ceeef723e311b488f2c9
donovanpoland <dpoland@byupathway.edu>
2025-07-14 22:52:47 -0600

    aria hidden updated on different screen sizes for nav button

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,11 +1,12 @@
 const navButton = document.querySelector('#nav-button');
 const navBar = document.querySelector('#nav-bar');
-let width = window.innerWidth;
 
+updateAriaHidden();
 
+//hide navigation
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
-    navBar.classList.toggle('show');
+    navBar.classList.toggle('show');  
 
 });
 
@@ -14,4 +15,17 @@ window.addEventListener("resize", () => {
     navButton.classList.remove('show');
     navBar.classList.remove('show');
 
+    updateAriaHidden();
 });
+
+function updateAriaHidden() {
+    // min width set in media.css file
+    const rem = parseFloat(getComputedStyle(document.documentElement).fontSize) * 47;
+    const width = window.innerWidth;
+
+    if (width > rem) {
+        navButton.setAttribute("aria-hidden", "true");
+    } else {
+        navButton.setAttribute("aria-hidden", "false");
+    }
+}
