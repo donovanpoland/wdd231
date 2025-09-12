3fab504f13f51b69b81811d10f6efe1e605a7ff1
donovanpoland <dpoland@byupathway.edu>
2025-09-11 22:28:44 -0600

    list view grid view button listeners / modified viewable data in list view

--- chamber/scripts/scipts.js
+++ chamber/scripts/scipts.js
@@ -1,6 +1,9 @@
 // Add class for showing navigation on hamburger button click
 const navButton = document.querySelector('#ham-btn');
-const navBar = document.querySelector('#nav-bar')
+const navBar = document.querySelector('#nav-bar');
+const girdButton = document.querySelector('#grid-view');
+const listButton = document.querySelector('#list-view');
+const display = document.querySelector("#directory");
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
@@ -12,3 +15,16 @@ window.addEventListener("resize", () => {
   navButton.classList.remove('show');
   navBar.classList.remove('show');
 });
+
+girdButton.addEventListener("click", showGrid);
+listButton.addEventListener("click", showList);
+
+function showList() {
+	display.classList.add("directory-list");
+	display.classList.remove("directory-grid");
+}
+
+function showGrid() {
+  display.classList.add("directory-grid");
+  display.classList.remove("directory-list");
+}
