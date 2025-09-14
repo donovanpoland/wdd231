84aec82c700fee9f589da5bd49d27cafaa2c6784
donovanpoland <dpoland@byupathway.edu>
2025-09-14 17:23:19 -0600

    directory connection to database via js / card optimization for grid view / list view optimization / view button optimization /desktop navigation optimization

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,43 +4,116 @@ const jsonData = 'data/members.json' // File path
 const gridButton = document.querySelector('#grid-view');
 const listButton = document.querySelector('#list-view');
 const layout = document.querySelector("#directory");
-// Card selectors
-const card = document.querySelector('.card');
-const businessName = document.querySelector('.name');
-const membership = document.querySelector('.membership');
-const logo = document.querySelector('.logo');
-const description = document.querySelector('.desc');
-const street = document.querySelector('.street');
-const businessLocation = document.querySelector('.location');
-const email = document.querySelector('.email');
-const phone = document.querySelector('.phone');
-const website = document.querySelector('.url');
+
 
 
 
 gridButton.addEventListener("click", showGrid);
 listButton.addEventListener("click", showList);
 
+
+//add list class and remove grid class
 function showList() {
-	layout.classList.add("directory-list");
-	layout.classList.remove("directory-grid");
+  layout.classList.add("directory-list");
+  layout.classList.remove("directory-grid");
 }
 
+//add grid class and remove list class
 function showGrid() {
   layout.classList.add("directory-grid");
   layout.classList.remove("directory-list");
 }
 
-// async function
+// async function for fetching membership data
 async function fetchMemberData() {
   // Load/fetch data json file using json url path
   const response = await fetch(jsonData);
-  
+
   // Store the data
   const data = await response.json();
 
   // Log data(jason file) to console in table format to the console
   console.table(data.members);
+  displayData(data.members)
 }
+
+// For each card display info from data loaded
+function displayData(members) {
+
+  const cards = document.querySelectorAll('.card');
+  
+  members.forEach((member, index) => {
+
+    const card = cards[index];
+
+    // Set business name
+    const businessName = card.querySelector('.name');
+    businessName.textContent = member.name
+
+    // Set membership level
+    const membership = card.querySelector('.membership');
+    // Display star dependant on membership level
+    let imageUrl = getImageUrl(member.membership);
+    membership.setAttribute('src', imageUrl);
+    membership.setAttribute('alt', 'Membership level')
+
+    // Set logo
+    const logo = card.querySelector('.logo');
+    logo.setAttribute('src', member.logo);
+    logo.setAttribute('alt', `Logo for ${member.name}`);
+
+    // Set description
+    const description = card.querySelector('.desc');
+    description.textContent = member.description;
+
+    // Set address
+    const street = card.querySelector('.street');
+    const businessLocation = card.querySelector('.location');
+    street.textContent = member.address.street;
+    let loc = `${member.address.city} ${member.address.state} ${member.address.zip}`;
+    businessLocation.textContent = loc;
+
+    // Set phone number
+    const phone = card.querySelector('.phone');
+    phone.textContent = member.phone;
+
+    // Set website URL
+    const website = card.querySelector('.url');
+    let url = member.url;
+    let displayUrl = url.replace("https://", "");
+    website.setAttribute('href', url);
+    website.textContent = displayUrl;
+
+  });// End foreach
+}// End DisplayData function
+  
+  
+  
+ 
+  
+  
+  
+  
+
+
+  
+
+// Gets image url location for membership stars
+function getImageUrl(membership) {
+  let imageUrl = "";
+  if (membership === 1) {
+    imageUrl = 'images/membership/gold-star.svg';
+  }
+  else if (membership === 2) {
+    imageUrl = 'images/membership/silver-star.svg';
+  }
+  else if (membership === 3) {
+    imageUrl = 'images/membership/bronze-star.svg';
+  }
+  else imageUrl = "";
+
+  return imageUrl;
+}
+
 
 fetchMemberData();
