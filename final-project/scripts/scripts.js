20efcbf31a5aae0a96ad650d331d77eca5dd3ecc
donovanpoland <dpoland@byupathway.edu>
2025-10-01 23:54:01 -0600

    base file structure and navigation for final project / site plan bas structure / site proposal set

---
+++ final-project/scripts/scripts.js
@@ -0,0 +1,14 @@
+// Add class for showing navigation on hamburger button click
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+// Listen for window resize and remove show class
+window.addEventListener("resize", () => {
+  navButton.classList.remove('show');
+  navBar.classList.remove('show');
+});
